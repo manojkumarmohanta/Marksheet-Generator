f584dfab50bf65ea4d698be088661b93a169e244
manojkumarmohanta <manojkumarmohanta1319@gmail.com>
2023-10-10 10:58:39 +0530

    in 10-10-23

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect('localhost','root','','niis_db');
 <div class="left-sidebar bg-black-300 box-shadow ">
                         <div class="sidebar-content">
                             <div class="user-info closed">
-                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="Admin" class="img-circle profile-img">
                                 <h6 class="title">Admin</h6>
                                 <small class="info">Administrator</small>
                             </div>
